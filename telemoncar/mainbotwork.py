1f546311c99bc85f411d56b922525d55e080dbcc
Rocket <hariharanm207@gmail.com>
2023-12-27 11:59:48 +0530

    2 steps before delivery completed

--- telemoncar/mainbotwork.py
+++ telemoncar/mainbotwork.py
@@ -18,6 +18,8 @@ user_client.connect()
 if not user_client.is_user_authorized():
     user_client.send_code_request(phone)
     user_client.sign_in(phone, input('Enter the code: '))
+
+
 async def main():
     
     # Create bot client
@@ -72,28 +74,6 @@ async def main():
         ]
         await event.respond(payment_message, buttons=buttons)
 
-    @bot_client.on(events.CallbackQuery(pattern=b'service_2'))
-    async def service_2(event):
-        service_details = "Details for Service 2:\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit."
-        payment_message = "To proceed with Service 2, please make the payment."
-        await event.respond(service_details)
-        await event.respond(payment_message)
-
-    @bot_client.on(events.CallbackQuery(pattern=b'service_3'))
-    async def service_3(event):
-        service_details = "Details for Service 3:\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit."
-        payment_message = "To proceed with Service 3, please make the payment."
-        await event.edit(service_details)
-        await event.respond(payment_message)
-
-    @bot_client.on(events.CallbackQuery(pattern=b'service_4'))
-    async def service_4(event):
-        service_details = "Details for Service 4:\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit."
-        payment_message = "To proceed with Service 4, please make the payment."
-        await event.edit(service_details)
-        await event.respond(payment_message)
-
-    
     @bot_client.on(events.CallbackQuery(pattern=b'pay_service_1'))
     async def pay_service_1(event):
         confirmation_message = "Payment for Football confirmed! Thank you." 
@@ -125,6 +105,166 @@ async def main():
         decline_message = "Kindly, contact the admin" 
         await event.edit(decline_message)
 
+    @bot_client.on(events.CallbackQuery(pattern=b'service_2'))
+    async def service_2(event):
+        basketballOption = (
+            "By purchasing this service, you get access to: \n"
+            "\t - Toms Tips Premium Basketball Group \U0001F3C0 \n"
+            "\u25A0 In this group, we will be posting bets on the lower leagues of Basketball (No NBA). \n"
+            "\u25A0 Most bets are placed within 48 hours of the match start, sometimes even minutes before. This way the turnover will be fast, and thus you can compound your bank quickly. \n"
+            "\u25A0 When available, we will be tracking the closing lines of our bets in the spreadsheet. \n This remains a key indicator for our long term success, even on lower limit leagues. You can expect 150-200 bets a month in this group, and we expect an average ROI% of 10% \n"
+            "\u25A0 If you have any questions, feel free to reach out to @THKTipsNBA on twitter. \n" 
+            "\u2023 By purchasing this service, you agree to the Terms and Conditions. \n"
+        )
+        await event.respond(basketballOption)
+        payment_message = "To proceed with Basketball, please make the payment."
+        buttons = [
+            [
+                Button.inline("Pay Now", b'pay_service_2'),
+            ],
+           
+        ]
+        await event.respond(payment_message, buttons=buttons)
+
+    @bot_client.on(events.CallbackQuery(pattern=b'pay_service_2'))
+    async def pay_service_2(event):
+        confirmation_message = "Payment for Basketball confirmed! Thank you." 
+        await event.edit(confirmation_message)
+        acceptOrDeclineMessage = "<a href='https://docs.google.com/document/d/e/2PACX-1vTvzil-qCjU2kUQ2NWDGiOcRNVzMPV7_fWK_bJLUAQOurmEZ3D4R712xJ5ayz0vYaEaycMDcmmOCM3S/pub'>Terms and Conditions</a>"
+        buttons = [
+            [
+                Button.inline("Accept", b'add_members_service_2'),
+                Button.inline("Decline", b'decline_service_2'),
+            ],
+           
+        ]
+        await event.respond(acceptOrDeclineMessage, buttons=buttons, parse_mode='html')
+    
+    @bot_client.on(events.CallbackQuery(pattern=b'add_members_service_2'))
+    async def add_members_service_2(event):
+        targetGroupId = 2096266635 
+        targetGroupAccessHash = -198665311552621838
+        target_group_entity = InputPeerChannel(targetGroupId,targetGroupAccessHash)
+        sender = await event.get_sender()
+        user_to_add = InputPeerUser(sender.id, sender.access_hash)
+        await user_client(InviteToChannelRequest(target_group_entity,[user_to_add]))
+        accept_message = "You have successfully added to Basketball group" 
+        await event.edit(accept_message)
+
+
+    @bot_client.on(events.CallbackQuery(pattern=b'decline_service_2'))
+    async def decline_service_2(event):
+        decline_message = "Kindly, contact the admin" 
+        await event.edit(decline_message)
+    
+    @bot_client.on(events.CallbackQuery(pattern=b'service_3'))
+    async def service_3(event):
+        nbaOption = (
+            "By purchasing this service, you get access to: \n"
+            "\t - Toms Tips Premium NBA Props Group \U0001F3C6 \n"
+            "\u25A0 In this group we will be posting NBA Player Prop bets. \n"
+            "\u25A0 Most of the bets will be placed close to tip-off (start of the match), but we also take earlier lines when available. Because of this, even people from EU that don’t want to stay up late can still enjoy a part of our bets. Because the bets are posted close to tip-off, the turnover will be fast and thus you can compound your bank quickly. \n"
+            "\u25A0 When available, we will be tracking the closing lines of our bets in the spreadsheet. This remains a key indicator for our long term success. You can expect 150-200 bets a month in this group, and we expect an average ROI% of 10%. \n"
+            "\u25A0 If you have any questions, feel free to reach out to @THKTipsNBA on twitter." 
+            "\u2023 By purchasing this service, you agree to the Terms and Conditions \n"
+        )
+        await event.respond(nbaOption)
+        payment_message = "To proceed with NBA option, please make the payment."
+        buttons = [
+            [
+                Button.inline("Pay Now", b'pay_service_3'),
+            ],
+           
+        ]
+        await event.respond(payment_message, buttons=buttons)
+        
+    @bot_client.on(events.CallbackQuery(pattern=b'pay_service_3'))
+    async def pay_service_3(event):
+        confirmation_message = "Payment for NBA Props Group confirmed! Thank you." 
+        await event.edit(confirmation_message)
+        acceptOrDeclineMessage = "<a href='https://docs.google.com/document/d/e/2PACX-1vTvzil-qCjU2kUQ2NWDGiOcRNVzMPV7_fWK_bJLUAQOurmEZ3D4R712xJ5ayz0vYaEaycMDcmmOCM3S/pub'>Terms and Conditions</a>"
+        buttons = [
+            [
+                Button.inline("Accept", b'add_members_service_3'),
+                Button.inline("Decline", b'decline_service_3'),
+            ],
+           
+        ]
+        await event.respond(acceptOrDeclineMessage, buttons=buttons, parse_mode='html')
+    
+    @bot_client.on(events.CallbackQuery(pattern=b'add_members_service_3'))
+    async def add_members_service_3(event):
+        targetGroupId = 2096266635 
+        targetGroupAccessHash = -198665311552621838
+        target_group_entity = InputPeerChannel(targetGroupId,targetGroupAccessHash)
+        sender = await event.get_sender()
+        user_to_add = InputPeerUser(sender.id, sender.access_hash)
+        await user_client(InviteToChannelRequest(target_group_entity,[user_to_add]))
+        accept_message = "You have successfully added to NBA Props Group group" 
+        await event.edit(accept_message)
+
+
+    @bot_client.on(events.CallbackQuery(pattern=b'decline_service_3'))
+    async def decline_service_3(event):
+        decline_message = "Kindly, contact the admin" 
+        await event.edit(decline_message)
+
+
+    @bot_client.on(events.CallbackQuery(pattern=b'service_4'))
+    async def service_4(event):
+        allOption = (
+        "By purchasing this service, you get access to all three groups: \n"
+        "\u2022 Toms Tips Premium Football Group \n"
+        "\u2022 Toms Tips Premium NBA Props Group \n"
+        "\u2022 Toms Tips Premium Basketball Group \n"
+        "\u25A0 By choosing the all in one deal you get the full potential of our services and a nice discount! The football group is focused on higher limits, but bets will often be taken early. The lower league basketball group and the NBA props group have a higher volume and a quick turnover time, but lower limits. \n"
+        "\u25A0This unique combination of all three services will bring your betting game to the next level. We use closing line value as a key indicator in all our services, and this will bring us long term success. \n"
+        "\u25A0 If you have any questions, feel free to reach out to @THKTipsNBA on twitter. \n"
+        "\u2023 By purchasing this service, you agree to the Terms and Conditions. \n"
+        )
+        await event.respond(allOption)
+        payment_message = "To proceed with all three groups, please make the payment."
+        buttons = [
+            [
+                Button.inline("Pay Now", b'pay_service_4'),
+            ],
+           
+        ]
+        await event.respond(payment_message, buttons=buttons)
+
+    @bot_client.on(events.CallbackQuery(pattern=b'pay_service_4'))
+    async def pay_service_4(event):
+        confirmation_message = "Payment for all three groups confirmed! Thank you." 
+        await event.edit(confirmation_message)
+        acceptOrDeclineMessage = "<a href='https://docs.google.com/document/d/e/2PACX-1vTvzil-qCjU2kUQ2NWDGiOcRNVzMPV7_fWK_bJLUAQOurmEZ3D4R712xJ5ayz0vYaEaycMDcmmOCM3S/pub'>Terms and Conditions</a>"
+        buttons = [
+            [
+                Button.inline("Accept", b'add_members_service_4'),
+                Button.inline("Decline", b'decline_service_4'),
+            ],
+           
+        ]
+        await event.respond(acceptOrDeclineMessage, buttons=buttons, parse_mode='html')
+    
+    @bot_client.on(events.CallbackQuery(pattern=b'add_members_service_4'))
+    async def add_members_service_4(event):
+        targetGroupId = 2096266635 
+        targetGroupAccessHash = -198665311552621838
+        target_group_entity = InputPeerChannel(targetGroupId,targetGroupAccessHash)
+        sender = await event.get_sender()
+        user_to_add = InputPeerUser(sender.id, sender.access_hash)
+        await user_client(InviteToChannelRequest(target_group_entity,[user_to_add]))
+        accept_message = "You have successfully added to all three groups" 
+        await event.edit(accept_message)
+
+
+    @bot_client.on(events.CallbackQuery(pattern=b'decline_service_4'))
+    async def decline_service_4(event):
+        decline_message = "Kindly, contact the admin" 
+        await event.edit(decline_message)
+
+    
+    
     # @bot_client.on(events.NewMessage(pattern='/sart1'))
     # async def start1(event):
     #     targetGroupId = 2096266635 
@@ -137,6 +277,8 @@ async def main():
     #     print("##################################")
     #     await user_client(InviteToChannelRequest(target_group_entity,[user_to_add]))
     #     await event.respond('User added')
+
+
 
     print("Bot is running!")
     await bot_client.run_until_disconnected()
